0f25d28dc8bb7de484b5ba30ff33b3c8ffe72c90
muhammadarif <mukarramarif03@gmail.com>
2025-11-13 17:50:35 -0500

    added descriptor code

---
+++ Main/MukkiGamesEngine/vulkan/Descriptors/VkDescriptor.cpp
@@ -0,0 +1,82 @@
+#include "VkDescriptor.h"
+#include <stdexcept>
+
+VkDescriptorBoss::VkDescriptorBoss(const Device* device, uint32_t maxSets)
+{
+	this->device = device;
+	createDescriptorPool(maxSets);
+}
+
+void VkDescriptorBoss::createDescriptorPool(uint32_t maxSets)
+
+{
+
+}
+
+void VkDescriptorBoss::createDescriptorSets(VkDescriptorSetLayout descriptorSetLayout, uint32_t count, std::vector<VkDescriptorSet>& descriptorSets)
+{
+    std::vector<VkDescriptorSetLayout> layouts(count, descriptorSetLayout);
+    VkDescriptorSetAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.descriptorPool = descriptorPool;
+    allocInfo.descriptorSetCount = static_cast<uint32_t>(count);
+    allocInfo.pSetLayouts = layouts.data();
+
+    descriptorSets.resize(count);
+    if (vkAllocateDescriptorSets(device->getDevice(), &allocInfo, descriptorSets.data()) != VK_SUCCESS) {
+        throw std::runtime_error("failed to allocate descriptor sets!");
+    }
+
+    for (size_t i = 0; i < count; i++) {
+        VkDescriptorBufferInfo bufferInfo{};
+        bufferInfo.buffer = uniformBuffers[i];
+        bufferInfo.offset = 0;
+        bufferInfo.range = sizeof(UniformBufferObject);
+
+        VkDescriptorImageInfo imageInfo{};
+        imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+        imageInfo.imageView = textureImageView;
+        imageInfo.sampler = textureSampler;
+
+        VkDescriptorBufferInfo ssboInfo{};
+        ssboInfo.buffer = bulletSSBO.buffer;
+        ssboInfo.offset = 0;
+        ssboInfo.range = VK_WHOLE_SIZE;
+
+        std::array<VkWriteDescriptorSet, 3> descriptorWrites{};
+
+        descriptorWrites[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptorWrites[0].dstSet = descriptorSets[i];
+        descriptorWrites[0].dstBinding = 0;
+        descriptorWrites[0].dstArrayElement = 0;
+        descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+        descriptorWrites[0].descriptorCount = 1;
+        descriptorWrites[0].pBufferInfo = &bufferInfo;
+
+        descriptorWrites[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptorWrites[1].dstSet = descriptorSets[i];
+        descriptorWrites[1].dstBinding = 1;
+        descriptorWrites[1].dstArrayElement = 0;
+        descriptorWrites[1].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+        descriptorWrites[1].descriptorCount = 1;
+        descriptorWrites[1].pImageInfo = &imageInfo;
+
+        descriptorWrites[2].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptorWrites[2].dstSet = descriptorSets[i];
+        descriptorWrites[2].dstBinding = 2;
+        descriptorWrites[2].dstArrayElement = 0;
+        descriptorWrites[2].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+        descriptorWrites[2].descriptorCount = 1;
+        descriptorWrites[2].pBufferInfo = &ssboInfo;
+
+        vkUpdateDescriptorSets(device->getDevice(), static_cast<uint32_t>(descriptorWrites.size()), descriptorWrites.data(), 0, nullptr);
+    }
+}
+
+void VkDescriptorBoss::destroyDescriptorPool()
+{
+}
+
+VkDescriptorBoss::~VkDescriptorBoss()
+{
+}
---
+++ Main/MukkiGamesEngine/vulkan/Descriptors/VkDescriptor.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vulkan/vulkan.h>
+#include "../Core/VkDevice.h"
+#include <vector>
+#include <array>
+#include <glm/glm.hpp>
+class VkDescriptorBoss
+{
+public:
+	struct uniformBufferObject {
+		alignas(16) glm::mat4 model;
+		alignas(16) glm::mat4 view;
+		alignas(16) glm::mat4 proj;
+	};
+	VkDescriptorBoss(const Device* device, uint32_t maxSets);
+	void createDescriptorPool(uint32_t maxSets);
+	void createDescriptorSets(VkDescriptorSetLayout descriptorSetLayout, uint32_t count, std::vector<VkDescriptorSet>& descriptorSets);
+	void destroyDescriptorPool();
+	~VkDescriptorBoss();
+
+private:
+	VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
+	Device* device = nullptr;
+
+
+};
