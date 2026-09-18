0b58ce5bad3798816d2fbc569821ee7984dfa608
mukarramarif <mukarramarif03@gmail.com>
2026-09-18 14:48:32 -0400

    Update conanfile.py

--- Main/conanfile.py
+++ Main/conanfile.py
@@ -6,13 +6,13 @@ class MukkiGamesEngine(ConanFile):
     requires = [
         "glfw/3.4",
         "imguizmo/cci.20231114",
-        "glm/1.0.1",
+        "glm/1.0.3",
         "nlohmann_json/3.12.0",
         "tinygltf/2.8.23",
         "stb/cci.20240531",
         "gli/cci.20210515",
         "tracy/0.13.1",
-        "ktx/4.4.2"
+        "ktx/4.3.2"
     ]
     generators = "CMakeDeps", "CMakeToolchain"
 
